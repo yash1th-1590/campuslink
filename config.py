eaf57a3226657a5792c71a1021b3b73d5cbe4bbe
Your Name <battuyashwanthreddy@gmail.com>
2026-07-12 16:02:47 +0530

    Updated

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ load_dotenv()
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'campuslink-secret-key-2024')
     
-    SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://{os.getenv('MYSQL_USER')}:{os.getenv('MYSQL_PASSWORD')}@{os.getenv('MYSQL_HOST')}:{os.getenv('MYSQL_PORT')}/{os.getenv('MYSQL_DB')}"
+    SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://{os.getenv('MYSQL_USER')}:{os.getenv('MYSQL_PASSWORD')}@{os.getenv('MYSQL_HOST')}:{os.getenv('MYSQL_PORT')}/{os.getenv('MYSQL_DB')}?ssl_verify_cert=true"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     
     LOGIN_VIEW = 'login'
